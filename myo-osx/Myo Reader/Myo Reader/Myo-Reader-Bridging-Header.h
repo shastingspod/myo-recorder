73878b399e839aa16f88941ab5823969d5da6882
Sam Hastings <shastings219@gmail.com>
2016-07-10 19:08:57 -0400

    moving around files to set up iOS and OS X file structure

---
+++ myo-osx/Myo Reader/Myo Reader/Myo-Reader-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Myo-Reader-Bridging-Header.h
+//  Myo Reader
+//
+//  Created by Sam Hastings on 1/23/16.
+//  Copyright © 2016 Sam Hastings. All rights reserved.
+//
+
+#ifndef Myo_Reader_Bridging_Header_h
+#define Myo_Reader_Bridging_Header_h
+
+#import <MyoKit/MyoKit.h>
+
+#endif /* Myo_Reader_Bridging_Header_h */
