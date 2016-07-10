9891a012425dc3acc4f8c43bf449176d5af549cf
Sam Hastings <shastings219@gmail.com>
2016-07-10 19:48:07 -0400

    successfully imported the myo framework into the swift project

---
+++ myo-ios/MyoReader/MyoReader/MyoKit-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  MyoKit-Bridging-Header.h
+//  MyoReader
+//
+//  Created by Sam Hastings on 7/10/16.
+//  Copyright © 2016 SEH. All rights reserved.
+//
+
+#ifndef MyoKit_Bridging_Header_h
+#define MyoKit_Bridging_Header_h
+
+#import "MyoKit/MyoKit.h"
+
+#endif /* MyoKit_Bridging_Header_h */
